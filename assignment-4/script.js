37c141c0d653df8507472665137f0d4bf882fa02
Raghav Vohra <66327225+vohraraghav22032000@users.noreply.github.com>
2021-08-23 23:03:20 +0530

    Update script.js

--- assignment-4/script.js
+++ assignment-4/script.js
@@ -4,7 +4,7 @@ names[1] = "John";
 names[2] = "jen";
 names[3] = "Jason";
 names[4] = "Paul";
-names[5] = "jrank";
+names[5] = "Frank";
 names[6] = "Larry";
 names[7] = "Paula";
 names[8] = "Laura";
@@ -12,7 +12,7 @@ names[9] = "Jim";
 
 for(var i=0;i<names.length;i++){
     if(names[i].charAt(0)==='J'  ||  names[i].charAt(0)==='j'){
-        console.log("Goodbye " + names[i]);
+        console.log("Good Bye " + names[i]);
     }
     else{
         console.log("Hello " + names[i]);
